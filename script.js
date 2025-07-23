fb6d77b913382dd68a356fba9109e9054a004e29
Thế Pháp <phapok0@gmail.com>
2025-07-23 15:22:46 +0700

    ,

--- script.js
+++ script.js
@@ -11,14 +11,15 @@ document.getElementById("checkout-form").addEventListener("submit", async functi
   formData.append("amount", amount);
 
   try {
-    await fetch("https://sheetdb.io/api/v1/jk595chfbj8m2", {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ data: { name, email, amount } })
-});
+//     await fetch("https://sheetdb.io/api/v1/jk595chfbj8m2", {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+//   body: JSON.stringify({ data: { name, email, amount } })
+// }
+// );
 
     // Chuyển sang Momo sau khi gửi xong
-    window.location.href = "https://nhantien.momo.vn/phapok0";
+    window.location.href = "thank_you.html";
   } catch (err) {
     alert("Gửi thông tin thất bại. Vui lòng thử lại.");
     console.error(err);
