519853256d723dd38aa50cd8b55903214e9e04a3
Thế Pháp <phapok0@gmail.com>
2025-07-23 13:53:03 +0700

    fix

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
 document.getElementById("checkout-form").addEventListener("submit", async function (e) {
   e.preventDefault();
 
-  const name = document.getElementById("name").value;
-  const email = document.getElementById("email").value;
-  const amount = 19990; // đơn giá
-
-  const data = { name, email, amount };
-
   try {
-    // Gửi dữ liệu tới Google Apps Script
-    await fetch("https://script.google.com/macros/s/AKfycbwTF-siO0qUn2BFTzfyWs2SUTDsa6v719XHjMBwb1ezq-q3pd-XCINYCoJGcypP-UOB/exec", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(data)
-    });
+    const formData = new URLSearchParams();
+formData.append("name", name);
+formData.append("email", email);
+formData.append("amount", amount);
+
+await fetch("https://script.google.com/macros/s/AKfycbwTF-siO0qUn2BFTzfyWs2SUTDsa6v719XHjMBwb1ezq-q3pd-XCINYCoJGcypP-UOB/exec", {
+  method: "POST",
+  body: formData
+});
+
 
     // Redirect sang Momo
     window.location.href = "https://nhantien.momo.vn/phapok0"; // link Momo cố định của bạn
